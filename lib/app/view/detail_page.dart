92042099b9e2113ee22d5756d17426cdd9b6b070
ramadhanialfarizi <72063724+ramadhanialfarizi@users.noreply.github.com>
2023-02-11 02:04:01 +0700

    first commit

---
+++ lib/app/view/detail_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../model/received_notification.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key});
+
+  static const routeName = '/detail_page';
+
+  @override
+  Widget build(BuildContext context) {
+    final ReceivedNotification arg =
+        ModalRoute.of(context)?.settings.arguments as ReceivedNotification;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title: ${arg.payload}'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:simple_notification/app/view/detail_page.dart';
+import 'package:simple_notification/app/view/home_page.dart';
+
+import 'utils/notification_helper.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final NotificationHelper notificationHelper = NotificationHelper();
+  await notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
+  notificationHelper.requestIOSPermissions(flutterLocalNotificationsPlugin);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => const HomePage(),
+        'detail': (context) => const DetailPage()
+      },
+    );
+  }
+}
